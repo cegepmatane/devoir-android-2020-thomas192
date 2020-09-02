8865361ffcdabb79cb5fd45f172a9d51ed562bfd
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-02 15:56:42 +0200

    ajout d'une fonction de remplissage de la liste des anniversaires

--- app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAnniversaire.java
+++ app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAnniversaire.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -13,12 +14,27 @@ import cgmatane.mobile.anniversaire.R;
 public class VueAnniversaire extends AppCompatActivity {
 
     protected ListView VueListeAnniversaire;
-    protected List<HashMap<String,String>> listeAnniversaire;
+    protected List<HashMap<String, String>> listeAnniversaire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_anniversaire);
         VueListeAnniversaire = (ListView)findViewById(R.id.VueListeAnniversaire);
+
+        listeAnniversaire = prepareListeAnniversaire();
+    }
+
+    private List<HashMap<String, String>> prepareListeAnniversaire() {
+        List<HashMap<String, String>> listeAnniversaire = new ArrayList<HashMap<String, String>>();
+
+        HashMap<String, String> anniversaire;
+
+        anniversaire = new HashMap<String, String>();
+        anniversaire.put("nom", "Thomas Saudemont");
+        anniversaire.put("date", "22/02/2001");
+        listeAnniversaire.add(anniversaire);
+
+        return listeAnniversaire;
     }
 }
