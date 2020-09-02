10327b736dc77e11ae2fbd8067535e9b35055494
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-02 15:38:58 +0200

    ajout d'une liste y mettre les anniversaires

--- app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAnniversaire.java
+++ app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAnniversaire.java
@@ -3,14 +3,22 @@ package cgmatane.mobile.anniversaire.vue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.HashMap;
+import java.util.List;
 
 import cgmatane.mobile.anniversaire.R;
 
 public class VueAnniversaire extends AppCompatActivity {
 
+    protected ListView VueListeAnniversaire;
+    protected List<HashMap<String,String>> listeAnniversaire;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_anniversaire);
+        VueListeAnniversaire = (ListView)findViewById(R.id.VueListeAnniversaire);
     }
 }
