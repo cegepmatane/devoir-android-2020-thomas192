b1b3493fff4eb676bbabb574b9dcda3ca55e0a97
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-02 17:20:57 +0200

    ajout bouton annuler fonctionnel sur la vue anniversaire

--- app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAjouterAnniversaire.java
+++ app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAjouterAnniversaire.java
@@ -3,6 +3,8 @@ package cgmatane.mobile.anniversaire.vue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cgmatane.mobile.anniversaire.R;
 
@@ -12,5 +14,20 @@ public class VueAjouterAnniversaire extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_ajouter_anniversaire);
+
+        Button vueAjouterAnniversaireActionAnnuler = (Button)findViewById(R.id.vueAjouterAnniversaireActionAnnuler);
+
+        vueAjouterAnniversaireActionAnnuler.setOnClickListener(
+                new View.OnClickListener() {
+
+                    public void onClick(View arg0) {
+                        naviguerRetourVueAnniversaire();
+                    }
+                }
+        );
+    }
+
+    public void naviguerRetourVueAnniversaire() {
+        this.finish();
     }
 }
