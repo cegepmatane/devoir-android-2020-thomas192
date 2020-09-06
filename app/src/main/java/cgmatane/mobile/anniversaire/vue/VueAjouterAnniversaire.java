23f3fd3a269e67ebadaddb371a146051bdc696b6
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-06 10:22:32 +0200

    formulaire ajouter anniversaire fonctionnel

--- app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAjouterAnniversaire.java
+++ app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAjouterAnniversaire.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
 
 import cgmatane.mobile.anniversaire.R;
+import cgmatane.mobile.anniversaire.donnee.AnniversaireDAO;
 
 public class VueAjouterAnniversaire extends AppCompatActivity {
+
+    protected EditText vueAjouterAnniversaireChampPrenomEtNom;
+    protected EditText vueAjouterAnniversaireChampDateDeNaissance;
+    protected AnniversaireDAO anniversaireDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,32 @@ public class VueAjouterAnniversaire extends AppCompatActivity {
                     }
                 }
         );
+
+        vueAjouterAnniversaireChampPrenomEtNom = (EditText)findViewById(R.id.vueModifierAnniversaireChampPrenomEtNom);
+        vueAjouterAnniversaireChampDateDeNaissance = (EditText)findViewById(R.id.vueModifierAnniversaireChampDateDeNaissance);
+
+        Button vueAjouterAnniversaireActionAjouter = (Button)findViewById(R.id.vueAjouterAnniversaireActionAjouter);
+
+        vueAjouterAnniversaireActionAjouter.setOnClickListener(
+                new View.OnClickListener() {
+
+                    public void onClick(View arg0) {
+                        enregistrerAnniversaire();
+                        naviguerRetourVueAnniversaire();
+                    }
+                }
+        );
+    }
+
+    public void enregistrerAnniversaire() {
+        HashMap<String, String> a;
+
+        a = new HashMap<String, String>();
+        a.put("prenomEtNom", vueAjouterAnniversaireChampPrenomEtNom.getText().toString());
+        a.put("dateDeNaissance", vueAjouterAnniversaireChampDateDeNaissance.getText().toString());
+
+        anniversaireDAO = anniversaireDAO.getInstance();
+        anniversaireDAO.ajouterAnniversaire(a);
     }
 
     public void naviguerRetourVueAnniversaire() {
