5830defceb253adb5fc9b4ab54120d78050369c5
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-16 16:47:21 +0200

    ajout fonction creation alarme si anniversaire aujourd'hui

--- app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAnniversaire.java
+++ app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAnniversaire.java
@@ -3,6 +3,9 @@ package cgmatane.mobile.anniversaire.vue;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,6 +15,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -21,6 +25,8 @@ import cgmatane.mobile.anniversaire.R;
 import cgmatane.mobile.anniversaire.donnee.AnniversaireDAO;
 import cgmatane.mobile.anniversaire.donnee.BaseDeDonnees;
 import cgmatane.mobile.anniversaire.modele.Anniversaire;
+
+import static android.app.AlarmManager.RTC;
 
 public class VueAnniversaire extends AppCompatActivity {
 
@@ -33,6 +39,7 @@ public class VueAnniversaire extends AppCompatActivity {
 
     static final public int ACTIVITE_AJOUTER_ANNIVERSAIRE = 1;
     static final public int ACTIVITE_MODIFIER_ANNIVERSAIRE = 2;
+    static final public int ACTIVITE_ALARME_ANNIVERSAIRE = 3;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -82,6 +89,8 @@ public class VueAnniversaire extends AppCompatActivity {
                     }
                 }
         );
+
+        creationAlarmeAnniversaire();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -117,5 +126,27 @@ public class VueAnniversaire extends AppCompatActivity {
                 new int[] {android.R.id.text1, android.R.id.text2});
 
         vueListeAnniversaire.setAdapter(adapteur);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void creationAlarmeAnniversaire() {
+        // On déclenche une alarme qui lance une activité si un anniversaire a lieu aujourd'hui
+        for (Anniversaire a : listeAnniversaire) {
+
+            LocalDate dateActuelle = LocalDate.now();
+            LocalDate dateDeNaissance = LocalDate.parse(a.getDateDeNaissance());
+            LocalDate dateAnniversaire = dateDeNaissance.withYear(dateActuelle.getYear());
+
+            if (dateAnniversaire.isEqual(dateActuelle)) {
+                Intent intentionNaviguerAlarmeAnniversaire = new Intent(this, VueAlarmeAnniversaire.class);
+                intentionNaviguerAlarmeAnniversaire.putExtra("id", a.getId());
+
+                PendingIntent intentionEnAttente = PendingIntent.getActivity(this, ACTIVITE_ALARME_ANNIVERSAIRE,
+                        intentionNaviguerAlarmeAnniversaire, 0);
+
+                AlarmManager manager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+                manager.set(RTC, System.currentTimeMillis(), intentionEnAttente);
+            }
+        }
     }
 }
