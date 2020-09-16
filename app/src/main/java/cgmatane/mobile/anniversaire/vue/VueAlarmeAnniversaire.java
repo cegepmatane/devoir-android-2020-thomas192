b637906ec7efd9156128166b45488cabcdcf8a0c
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-16 15:49:40 +0200

    ajout VueAlarmeAnniversaire

---
+++ app/src/main/java/cgmatane/mobile/anniversaire/vue/VueAlarmeAnniversaire.java
@@ -0,0 +1,16 @@
+package cgmatane.mobile.anniversaire.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cgmatane.mobile.anniversaire.R;
+
+public class VueAlarmeAnniversaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_alarme_anniversaire);
+    }
+}
